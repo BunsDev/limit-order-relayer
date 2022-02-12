f38357b0ae1dd1c1e8d1104d8a085568c6b379b0
Buns <bunsthedev@gmail.com>
2022-02-12 16:23:10 -0600

    add: entities, constants

---
+++ src/entities/NativeCurrency.ts
@@ -0,0 +1,12 @@
+import { AbstractCurrency } from './AbstractCurrency'
+// import { Currency } from './Currency'
+import { Token } from './Token'
+
+/**
+ * Represents the native currency of the chain on which it resides, e.g.
+ */
+export abstract class NativeCurrency extends AbstractCurrency {
+  public readonly isNative: true = true
+  public readonly isToken: false = false
+  public readonly address: Token = new Token(250, '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83', 18, 'FTM')// = '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83'
+}
